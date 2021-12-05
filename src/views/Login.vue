fc8889cb04983cd6fe563d49c1895aa0a4df270f
nrcada829 <lai19930829@gmail.com>
2021-12-05 19:04:08 +0800

    登入與 Cookie

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -51,6 +51,8 @@ export default {
     signIn () {
       const api = `${process.env.VUE_APP_API}admin/signin`
       this.$http.post(api, this.user).then((res) => {
+        const { token, expired } = res.data
+        document.cookie = `hexToken=${token};expires=${new Date(expired)}`
         console.log(res)
       })
     }
